71f5e3a54951d295fdd3a08c20eecb5f3277706c
bencebachraty <bakybence@gmail.com>
2019-04-29 14:07:14 +0200

    added Utils class to create driver

---
+++ src/main/java/com/codecool/test/jiraglassdocumentation/utils/Utils.java
@@ -0,0 +1,20 @@
+package com.codecool.test.jiraglassdocumentation.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Utils {
+
+    public static final String baseURL = "https://jira2.codecool.codecanvas.hu/secure/Dashboard.jspa";
+
+    public static void setDriverPath() {
+        String driverProperty = System.getenv("DRIVER_PROPERTY");
+        String driverPath = System.getenv("DRIVER_PATH");
+    }
+
+    public static WebDriver createWebdriver() {
+        WebDriver driver = new ChromeDriver();
+
+        return driver;
+    }
+}
